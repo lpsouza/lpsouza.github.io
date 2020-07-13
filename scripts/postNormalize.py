cdef8fa21aa6dd52aaa43f6713ee82afff0a7713
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2020-07-13 01:08:30 -0300

    Changes in postNormalize script

--- scripts/postNormalize.py
+++ scripts/postNormalize.py
@@ -10,8 +10,13 @@ for file in files:
     print(join(dirPath, file))
     post = frontmatter.load(join(dirPath, file))
 
-    if hasattr(post.metadata, 'star'): del post.metadata['star']
-    if hasattr(post.metadata, 'permalink'): del post.metadata['permalink']
+    if "id" in post.metadata: del post.metadata['id']
+    if "guid" in post.metadata: del post.metadata['guid']
+    if "star" in post.metadata: del post.metadata['star']
+    if "permalink" in post.metadata: del post.metadata['permalink']
+    if "categories" in post.metadata: del post.metadata['categories']
+    if "aktt_tweeted" in post.metadata: del post.metadata['aktt_tweeted']
+    if "aktt_notify_twitter" in post.metadata: del post.metadata['aktt_notify_twitter']
 
     fileWriter = open(join(dirPath, file), 'w')
     fileWriter.write(frontmatter.dumps(post))
