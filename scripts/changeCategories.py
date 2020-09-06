a62a24b78c746e699cd7911901271f685d1face4
lpsouza <lpsouza@penguin.lxd>
2020-09-06 14:38:45 -0300

    Changes in changeCategories.py

--- scripts/changeCategories.py
+++ scripts/changeCategories.py
@@ -11,7 +11,14 @@ def changeCategory(file):
     post = frontmatter.load(file)
 
     print("Title: " + post.metadata['title'])
-    print("Categories:\n- Blogging\n -Tech\n -Linux\n -Games\n\n(type \"del\" for deletion or \"cancel\" to cancel change category operation\n")
+    print(post.content[:150] + "...")
+    print("\nCategories:")
+    categories = [p.metadata['category'] for p in posts]
+    categories = set(categories)
+    categories = (list(categories))
+    for category in categories:
+        print("- " + category)
+    print("(type \"del\" for deletion or \"cancel\" to cancel change category operation\n")
     print("Actual category: " + post.metadata['category'])
     newCategory = input("New category: ")
     post.metadata['category'] = newCategory
@@ -27,6 +34,7 @@ def changeCategory(file):
 dirPath = "_posts/"
 files = [f for f in listdir(dirPath) if isfile(join(dirPath, f))]
 files.sort()
+posts = [frontmatter.load(join(dirPath, f)) for f in files]
 
 mainMenu = ConsoleMenu("Category manager")
 
@@ -34,7 +42,7 @@ for file in files:
     post = frontmatter.load(join(dirPath, file))
     title = post.metadata['title']
     category = post.metadata['category']
-    title = ("[" + category + "] " + title[:50] + "..") if len(title) > 50 else "[" + category + "] " + title
+    title = ("[" + category + "] " + title[:40] + "...") if len(title) > 40 else "[" + category + "] " + title
     item = FunctionItem(title, changeCategory, [join(dirPath, file)])
     mainMenu.append_item(item)
 
