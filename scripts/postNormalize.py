ded7be30c26ebf33400dc7457e1939f0e574c48a
lpsouza <lpsouza@penguin.lxd>
2020-09-06 02:03:19 -0300

    Changes to revert postNormalize.py to normal functions

--- scripts/postNormalize.py
+++ scripts/postNormalize.py
@@ -1,3 +1,5 @@
+# Script to "normalize" all posts
+# (remove unnecessary metadata options from my old Wordpress blog)
 import frontmatter
 
 from os import listdir, remove
@@ -9,29 +11,16 @@ files = [f for f in listdir(dirPath) if isfile(join(dirPath, f))]
 for file in files:
     post = frontmatter.load(join(dirPath, file))
 
-    # print(post.metadata['category'])
+    if "id" in post.metadata: del post.metadata['id']
+    if "guid" in post.metadata: del post.metadata['guid']
+    if "star" in post.metadata: del post.metadata['star']
+    if "permalink" in post.metadata: del post.metadata['permalink']
+    if "categories" in post.metadata: del post.metadata['categories']
+    if "aktt_tweeted" in post.metadata: del post.metadata['aktt_tweeted']
+    if "aktt_notify_twitter" in post.metadata: del post.metadata['aktt_notify_twitter']
+    if "tumblrize_post-id" in post.metadata: del post.metadata['tumblrize_post-id']
+    if "tumblrize_post-type" in post.metadata: del post.metadata['tumblrize_post-type']
 
-    if post.metadata['category'] == "blog":
-        print("Title: " + post.metadata['title'])
-        print("Categories: Blogging, Tech, Linux, Games")
-        newCategory = raw_input("New category: ")
-        post.metadata['category'] = newCategory
-
-    # if "id" in post.metadata: del post.metadata['id']
-    # if "guid" in post.metadata: del post.metadata['guid']
-    # if "star" in post.metadata: del post.metadata['star']
-    # if "permalink" in post.metadata: del post.metadata['permalink']
-    # if "categories" in post.metadata: del post.metadata['categories']
-    # if "aktt_tweeted" in post.metadata: del post.metadata['aktt_tweeted']
-    # if "aktt_notify_twitter" in post.metadata: del post.metadata['aktt_notify_twitter']
-    # if "tumblrize_post-id" in post.metadata: del post.metadata['tumblrize_post-id']
-    # if "tumblrize_post-type" in post.metadata: del post.metadata['tumblrize_post-type']
-
-    # post.metadata['published'] = True
-
-        if newCategory == "del" :
-            remove(join(dirPath, file))
-        else :
-            fileWriter = open(join(dirPath, file), 'w')
-            fileWriter.write(frontmatter.dumps(post).encode('utf-8'))
-            fileWriter.close()
+    fileWriter = open(join(dirPath, file), 'w')
+    fileWriter.write(frontmatter.dumps(post).encode('utf-8'))
+    fileWriter.close()
