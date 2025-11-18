1d96858513978f1423372c3c6f204c3767948a2d
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2025-11-18 20:34:49 -0300

    Fix volume mounting in blog.sh for Jekyll Docker container

--- blog.sh
+++ blog.sh
@@ -3,4 +3,4 @@
 # This is a script to manage my jekyll blog using docker.
 
 export JEKYLL_VERSION=4.0
-docker run --rm --volume="$JEKYLL_BASEDIR:/srv/jekyll" -it jekyll/jekyll:$JEKYLL_VERSION jekyll $@
+docker run --rm -v "./:/srv/jekyll" -it -p 4000:4000 jekyll/jekyll:$JEKYLL_VERSION jekyll $@
